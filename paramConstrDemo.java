0e84fd19f443bf337dd9a7362ef33e54db45f606
krishnaprasath7 <109690738+krishnaprasath7@users.noreply.github.com>
2022-08-02 16:08:26 +0530

    Update paramConstrDemo.java

--- paramConstrDemo.java
+++ paramConstrDemo.java
@@ -1,4 +1,17 @@
+ class parastd {
+		int id;
+		String name;
 
+		parastd(int i,String n)
+		{
+		id=i;
+		name=n;
+		}
+
+		void display() {
+		System.out.println(id+" "+name);
+		}
+	}
 public class paramConstrDemo {
 	
 		public static void main(String[] args) {
